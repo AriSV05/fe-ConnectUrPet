75ca24085e44c4e11239f88dd568e654204949d2
AriSV05 <80506478+AriSV05@users.noreply.github.com>
2024-09-09 21:38:44 -0600

    Add files via upload

---
+++ ConnectUrPet/build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false // Library for Firebase
+}
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.7.7"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
